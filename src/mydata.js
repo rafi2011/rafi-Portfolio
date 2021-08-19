34ae7ed7633eda1f9e1d81746d3ea81ed5822770
rafi2011 <rnessa2011@gmail.com>
2021-08-19 19:54:30 -0400

    added my data file

---
+++ src/mydata.js
@@ -0,0 +1,57 @@
+let myData = {
+    "name": "Rafiun Nessa",
+    "role": "Full Stack Developer",
+    "linkedinId":"Rafiun Nessa",
+    "portfolio":[
+      {
+        "id":1,
+        "name":"Work Day Scheduler",
+        "description":"Daily task scheduling application",
+        "imgurl":"./images/WorkDayScheduler.jpeg",
+        "alt": "test",
+        "githubUrl": "",
+        "liveUrl": ""
+      },
+      {
+        "id":2,
+        "name":"Team Profile Generator",
+        "description":"CLI Interface which genereates HTML page using Inquirer",
+        "imgurl":"./images/TeamProfileGenerator.gif",
+        "alt": "test",
+        "githubUrl": "",
+        "liveUrl": ""
+      },
+      {
+        "id":3,
+        "name":"Note Taker",
+        "description":"Rrite and save notes using an Express.js back end and will save and retrieve note data from a JSON file.",  
+        "imgurl":"./images/NoteTaker.gif",
+        "alt": "test",
+        "githubUrl": "",
+        "liveUrl": ""
+      },
+      {
+        "id":4,
+        "name":"ReadMe Generator",
+        "description":"CLI which takes user inputted answers to prompted questions and generates a ReadMe file",
+        "imgurl":"./images/ReadMeGenerator.gif",
+        "alt": "test",
+        "githubUrl": "",
+        "liveUrl": ""
+      }
+      ,
+      {
+        "id":5,
+        "name":"Tech Blog",
+        "description":"CMS-style blog connected to MySQL using the MVC design paradigm",
+        "imgurl":"./images/TechBlog.jpeg",
+        "alt": "test",
+        "githubUrl": "",
+        "liveUrl": ""
+      }
+      ,
+      
+    ]
+  }
+  
+export default myData;
